7504398a7a905a81150a35327c5f79a9eb6bf595
Vyomesh Dangi <vyomeshdangi.official@gmail.com>
2026-05-09 12:11:20 +0530

    Add files via upload

--- HigherOrLower.py
+++ HigherOrLower.py
@@ -9,10 +9,9 @@ there will be three levels.. as level increases range of numbers will too""")
 
 yn = input("start game ? (y/n) ")
 
-def game_level_1():
-    attempts = 10
-    winningnumber = random.randint(1, 10)
-    print("The range is 1-10")
+def game(range,attempts):
+    winningnumber = random.randint(1,range)
+    print(f"The range is 1-{range}")
     print("guessing number......")
     print("done !")
     # print(winningnumber)   
@@ -38,75 +37,24 @@ def game_level_1():
         print("all attempts used! game over !")
         print(f"the answer was {winningnumber}")
         
-def game_level_2():
-    attempts = 10
-    winningnumber = random.randint(1, 50)
-    print("The range is 1-50")
-    print("guessing number......")
-    print("done !")
-    # print(winningnumber)   
-    guess = None
-    
-    while guess != winningnumber and attempts != 0:
-        guess = int(input(f"Enter your guess ({attempts} tries left): "))
-        attempts -= 1 
 
-        if guess > winningnumber:
-            print("lower !")
-            
-        elif guess < winningnumber:
-            print("higher!")
-
-        else :
-            print("CORRECT !")
-
-    if guess == winningnumber:
-        print(f"you won! with {attempts} number of attempts left")
-
-    else:
-        print("all attempts used! game over !")
-        print(f"the answer was {winningnumber}")
-
-def game_level_3():
-    attempts = 10
-    winningnumber = random.randint(1, 100)
-    print("The range is 1-100")
-    print("guessing number......")
-    print("done !")
-    # print(winningnumber)   
-    guess = None
-    
-    while guess != winningnumber and attempts != 0:
-        guess = int(input(f"Enter your guess ({attempts} tries left): "))
-        attempts -= 1 
-
-        if guess > winningnumber:
-            print("lower !")
-            
-        elif guess < winningnumber:
-            print("higher!")
-
-        else :
-            print("CORRECT !")
-
-    if guess == winningnumber:
-        print(f"you won! with {attempts} number of attempts left")
-
-    else:
-        print("all attempts used! game over !")
-        print(f"the answer was {winningnumber}")
-    
 if yn == "y":
-    levelinput = input("which level (1,2 or 3) : ")
+    levelinput = input("which level (1,2,3 or IMPOSSIBLE) : ")
 
     if levelinput == "1":
-        game_level_1()
+        game(10,10)
 
     elif levelinput == "2":
-        game_level_2()
+        game(50,10)
 
     elif levelinput == "3":
-        game_level_3()
+        game(100,10)
+
+    elif levelinput == "IMPOSSIBLE":
+        game(1000000000000000000000000,1000000)
 
     else:
-        print("try again !")    
+        print("try again !") 
+
+else:
+    print("why did you even run if you don't want to play ..?")   
